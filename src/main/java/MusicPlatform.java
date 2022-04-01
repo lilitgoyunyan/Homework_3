40135d6a752a211e7f24e2300edaea7ad785c90d
Lilit Goyunyan <lilitgoyunyan@Lilits-MacBook-Air.local>
2022-04-01 19:31:26 +0400

    First Commit

--- src/main/java/MusicPlatform.java
+++ src/main/java/MusicPlatform.java
@@ -14,7 +14,7 @@ public class MusicPlatform {
         return songs;
     }
 
-    
+
     public Object findSong(String name, String singer) {
         for (int i = 0; i < songs.size(); i++) {
             if (songs.get(i).getSinger().equals(name) && songs.get(i).getSinger().equals(singer)) {
@@ -24,18 +24,21 @@ public class MusicPlatform {
         return true;
     }
 
-    public ArrayList<Song> addSong(Song s) {
+    public boolean addSong(Song s) {
         if (!songs.contains(s)) {
             songs.add(s);
+            return true;
         }
-        return songs;
+        return false;
     }
 
-    public ArrayList<Song> removeSong(Song s) {
-        if (!songs.contains(s)) {
+    public boolean removeSong(Song s) {
+        if (songs.contains(s)) {
             songs.remove(s);
+            return true;
+
         }
-        return songs;
+        return false;
     }
 
 }
